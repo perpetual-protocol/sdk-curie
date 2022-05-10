3899a2224c33129e31be25089350d30df0abe0cf
Philip Chen <lsps9150414@gmail.com>
2022-05-10 17:49:27 +0800

    build: (WIP) prep to release for dev 1

--- src/network/constants.ts
+++ src/network/constants.ts
@@ -1,25 +1,30 @@
 import { METADATA_URL_OVERRIDE_OPTIMISM, METADATA_URL_OVERRIDE_OPTIMISM_KOVAN } from "../constants"
 
-import MainMetadataOptimism from "@perp/curie-deployments/optimism/core/metadata.json"
+// import MainMetadataOptimism from "@perp/curie-deployments/optimism/core/metadata.json"
 import MainMetadataOptimismKovan from "@perp/curie-deployments/optimism-kovan/core/metadata.json"
-import PeripheryMetadataOptimism from "@perp/curie-periphery/metadata/optimism.json"
+// import PeripheryMetadataOptimism from "@perp/curie-periphery/metadata/optimism.json"
 import PeripheryMetadataOptimismKovan from "@perp/curie-periphery/metadata/optimismKovan.json"
 
 export enum SupportedChainId {
     OPTIMISM_KOVAN = MainMetadataOptimismKovan.chainId,
-    OPTIMISM = MainMetadataOptimism.chainId,
+    // OPTIMISM = MainMetadataOptimism.chainId,
 }
 
 export const CuriePeripheryMetadataMap = {
     [SupportedChainId.OPTIMISM_KOVAN]: PeripheryMetadataOptimismKovan,
-    [SupportedChainId.OPTIMISM]: PeripheryMetadataOptimism,
+    // [SupportedChainId.OPTIMISM]: PeripheryMetadataOptimism,
 }
 
 export const MetadataUrlByChainId = {
     [SupportedChainId.OPTIMISM_KOVAN]: METADATA_URL_OVERRIDE_OPTIMISM_KOVAN
         ? METADATA_URL_OVERRIDE_OPTIMISM_KOVAN
         : "https://metadata.perp.exchange/v2/optimism-kovan.json",
-    [SupportedChainId.OPTIMISM]: METADATA_URL_OVERRIDE_OPTIMISM
-        ? METADATA_URL_OVERRIDE_OPTIMISM
-        : "https://metadata.perp.exchange/v2/optimism.json",
+    // [SupportedChainId.OPTIMISM]: METADATA_URL_OVERRIDE_OPTIMISM
+    //     ? METADATA_URL_OVERRIDE_OPTIMISM
+    //     : "https://metadata.perp.exchange/v2/optimism.json",
 }
+
+// TODO: gen type with dev1
+// TODO: select kovan as SupportedChainId & CuriePeripheryMetadata
+// TODO: select kovan dev1 as MetadataUrl
+// TODO: update package versioning
