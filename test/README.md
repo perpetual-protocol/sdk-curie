b09fa0e3bd85821faabd4c09ce54590b90e8d353
Philip Chen <lsps9150414@gmail.com>
2022-05-23 17:24:44 +0800

    test: add more PerpetualProtocol tests

---
+++ test/README.md
@@ -0,0 +1,4 @@
+# Perpetual Protocol Curie SDK Example Tests
+
+The tests also serve as a collection of demo code in pure Javascript for common scenarios.
+Please see the `./core/` folder for the primary business logic.
--- test/core/PerpetualProtocol.test.ts
+++ test/core/PerpetualProtocol.test.ts
@@ -1,7 +1,21 @@
-import { PerpetualProtocol, SupportedChainIds } from "../../src"
+import { Signer } from "ethers"
+import {
+    PerpetualProtocol,
+    PerpetualProtocolConnected,
+    PerpetualProtocolInitialized,
+    SupportedChainIds,
+} from "../../src"
 
 describe("PerpetualProtocol", () => {
     let perp: PerpetualProtocol
+    const CHAIN_ID = SupportedChainIds.OPTIMISM_KOVAN
+    const PROVIDER_CONFIGS = [
+        {
+            rpcUrl: "https://kovan.optimism.io",
+        },
+    ]
+    // TODO:
+    // const SIGNER: Signer = undefined
 
     beforeAll(async () => {
         perp = new PerpetualProtocol({
@@ -15,7 +29,27 @@ describe("PerpetualProtocol", () => {
         await perp.init()
     })
 
-    describe("getMarket", () => {
+    describe("Using the PerpetualProtocol instance", () => {
+        it("initialize", async () => {
+            perp = new PerpetualProtocol({
+                chainId: CHAIN_ID,
+                providerConfigs: PROVIDER_CONFIGS,
+            })
+
+            expect(perp).toBeInstanceOf(PerpetualProtocol)
+            expect(perp.providerConfigs).toEqual(PROVIDER_CONFIGS)
+
+            expect(perp.hasInitialized).toEqual(false)
+            await perp.init()
+            expect(perp.hasInitialized).toEqual(true)
+        })
+
+        // it("connect signer", async () => {
+        //     expect(perp.hasConnected).toEqual(false)
+        //     await perp.connect({ signer: SIGNER })
+        //     expect(perp.hasConnected).toEqual(true)
+        // })
+
         it("getMarket", async () => {
             const market = perp.markets.getMarket({
                 tickerSymbol: "ETHUSD",
